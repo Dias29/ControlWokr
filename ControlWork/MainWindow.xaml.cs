bdedf2ad5454d915a889e492bc9c0b8aaa3dc9b1
УринбасаровД <@A-305-10>
2018-01-23 17:55:01 +0600

    first

---
+++ ControlWork/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ControlWork
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<Workers> workers;
+        public MainWindow()
+        {
+            InitializeComponent();
+            workers = new ObservableCollection<Workers>
+            {
+                new Workers
+                {
+                    
+                }
+            };
+            workerList.ItemsSource = workers;
+        }
+
+       
+
+        private void AddClick(object sender, RoutedEventArgs e)
+        {
+            workers.Add(new Workers
+            {
+
+            });
+        }
+
+        private void DleteClick(object sender, RoutedEventArgs e)
+        {
+
+            workers.RemoveAt(1);
+        }
+
+        //private void WorkerListBeginningEdit(object sender, DataGridBeginningEditEventArgs e)
+        //{
+        //    workerList.Ed
+        //}
+
+        //private void EditClick(object sender, RoutedEventArgs e)
+        //{
+        //    workerList.CurrentItem.
+        //}
+    }
+}
---
+++ ControlWork/Workers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlWork
+{
+    public class Workers
+    {
+        public string FName { get; set; }
+        public string LNamme { get; set; }
+        public string ThName { get; set; }
+        public string Status { get; set; }
+        
+    }
+}
